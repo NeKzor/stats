6b9474480f168bf0084240ed7d9fafb9a125f0a3
NeKz <nekzor@users.noreply.github.com>
2021-05-02 13:24:22 +0200

    Log requests to iverb

--- scripts/api/client.js
+++ scripts/api/client.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const { log } = require('../utils');
 
 class ResponseError extends Error {
     constructor(res) {
@@ -13,13 +14,17 @@ const BASE_API = 'https://board.iverb.me';
 class Portal2Boards {
     static async changelog(params) {
         params = params ? Object.entries(params).map((entry) => entry[0] + '=' + entry[1]) : [];
-        const res = await fetch(`${BASE_API}/changelog/json?${params.join('&')}`, {
+        const url = `${BASE_API}/changelog/json?${params.join('&')}`;
+
+        const res = await fetch(url, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'User-Agent': 'n^',
             },
         });
+
+        log.info(`[API CALL] GET : ${url} : ${res.status}`);
 
         if (res.status !== 200) {
             throw new ResponseError(res);
